935a8d375951eb7abdbfcb095cf0eaad050e899c
Brandon White <bwhite06@stac.edu>
2019-06-27 14:58:07 -0400

    added functionallity to button
    ---btn

--- js/index.js
+++ js/index.js
@@ -1,4 +1,35 @@
 
+document.querySelector("#btn").addEventListener("click", function(event){
+    event.preventDefault();
+    
+    let  x = document.querySelectorAll("body")[0].innerText;
+
+
+    console.log(x);
+
+        let y = document.querySelector("#search").value;
+        console.log(y);
+        let arr = x.split(" ");
+        console.log(arr)
+            let searchNew = arr.filter(el  => el.toLowerCase().indexOf(y.toLowerCase())!== -1
+        
+            );
+
+
+            alert(searchNew);
+        
+
+
+  });
+    
+
+    
+
+
+
+
+
+
 
 
 
