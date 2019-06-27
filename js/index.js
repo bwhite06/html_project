ce761a87902e38112376abc75ebdb494a2462b80
Dmitriy Kogan <52248346+dimokogan@users.noreply.github.com>
2019-06-27 16:07:33 -0400

    jquery/javascript stuff

--- js/index.js
+++ js/index.js
@@ -1,27 +1,39 @@
 
-document.querySelector("#btn").addEventListener("click", function(event){
-    event.preventDefault();
+// document.querySelector("#btn").addEventListener("click", function(event){
+   // event.preventDefault();
     
-    let  x = document.querySelectorAll("body")[0].innerText;
+ //   let  x = document.querySelectorAll("body")[0].innerText;
 
 
-    console.log(x);
-
-        let y = document.querySelector("#search").value;
-        console.log(y);
-        let arr = x.split(" ");
-        console.log(arr)
-            let searchNew = arr.filter(el  => el.toLowerCase().indexOf(y.toLowerCase())!== -1
+ //   console.log(x);
+//
+     //   let y = document.querySelector("#search").value;
+     //   console.log(y);
+   //     let arr = x.split(" ");
+    //   console.log(arr)
+      //      let searchNew = arr.filter(el  => el.toLowerCase().indexOf(y.toLowerCase())!== -1
         
-            );
+      //      );
 
 
-            alert(searchNew);
+    //        alert(searchNew);
         
 
 
-  });
-    
+ // });
+  
+ $(function() {
+    $('#text-search').bind('keyup change', function(ev) {
+        // pull in the new value
+        var searchNew = $(this).val();
+ 
+        // disable highlighting if empty
+        if ( searchNew ) {
+            // highlight the new term
+            $('body').highlight( searchNew );
+        }
+    });
+});
 
     
 
